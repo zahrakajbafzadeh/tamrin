65b3fe7cf19ad15994433bd29a04adea82526626
z.kajbafzade <z.kajbafzade@demisco.com>
2023-05-06 19:08:07 +0330

    metode negative and positive

--- tamrin2_32.java
+++ tamrin2_32.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class tamrin2_32 {
 
     public static void main(String[] args) {
-        System.out.println("kajbafzadeh");
+        Scanner input1 = new Scanner(System.in);
+        for (int x = 1; x <= 5; x++) {
+            try {
+                System.out.println("pleasr inter your number " + x + ": ");
+                int a = input1.nextInt();
+
+                if (a > 0) {
+                    System.out.println(a + " is positive");
+                } else if (a == 0) {
+                    System.out.println(a + " is zero");
+                } else System.out.println(a + " is negative");
+            } catch (Exception e) {
+                throw new RuntimeException("fvnj3");
+            }
+
+        }
     }
 }
+
